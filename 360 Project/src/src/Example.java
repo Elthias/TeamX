1b93eca62e142a85d646a8796d7a4b6a157e4ddb
Elthias <kpatt314@uw.edu>
2016-05-08 01:21:12 -0700

    Attempt to commit new changes of adding one line to the output.

--- 360 Project/src/src/Example.java
+++ 360 Project/src/src/Example.java
@@ -1,7 +1,16 @@
+package src;
 
 public class Example {
-
+	
+	/**
+	 * An example project to familiarize everyone with using github to upload and retrieve code.
+	 * 
+	 * @author Sarjodh Dhillon
+	 * @author Kenneth Patterson
+	 *
+	 */
 	public static void main(String[] theArgs) {
 		System.out.println("Hello");
+		System.out.println("Ho there!");
 	}
 }
