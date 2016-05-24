ae700a920d60d63674283c1f68bf61118e870ebe
sukhjawandhaz <sukhjawandhaz@gmail.com>
2016-05-24 15:40:10 -0700

    Update Main.java

--- 360 Project/src/src/Main.java
+++ 360 Project/src/src/Main.java
@@ -1,1 +1,29 @@
 
+package src;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	
+	private Main() {
+        throw new IllegalStateException();
+    }
+
+    /**
+     * The main method, invokes the GUI.
+     * 
+     * @param theArgs Command line arguments.
+     */
+    public static void main(final String[] theArgs) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Contestant_Page();
+                new Login_Page();
+            }
+        });
+    }
+}
+
+
