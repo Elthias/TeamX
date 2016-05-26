5d7ff3ba81a8c9c632923f54d28895aca74fc7e2
dovi.bergman@gmail.com <dovi.bergman@gmail.com>
2016-05-26 16:11:52 -0700

    Added logout methods an buttons to the otherwise empty judge_page and librarian_page classes

--- 360 Project/src/src/Judge_Page.java
+++ 360 Project/src/src/Judge_Page.java
@@ -1,1 +1,23 @@
+package src;
 
+import javax.swing.JPanel;
+
+/**
+ * This class contains the user page GUI for a Judge 
+ * 
+ * @author Dovi Bergman, Sukhpreet Singh Jawandha
+ */
+public class Judge_Page {
+	
+	JPanel myPanel;
+	
+	logout() {
+		button logoutButton = new JButton(new ActionListener() {
+			
+			public void ActionPerformed(ActionEvent theEvent) {
+				myPanel.hide();
+			}
+		});		
+	}
+	
+}
--- 360 Project/src/src/Librarian_Page.java
+++ 360 Project/src/src/Librarian_Page.java
@@ -1,1 +1,23 @@
+package src;
 
+import javax.swing.JPanel;
+
+/**
+ * This class contains the user page GUI for a Librarian 
+ * 
+ * @author Dovi Bergman, Sukhpreet Singh Jawandha
+ */
+public class Librarian_Page {
+	
+	JPanel myPanel;
+	
+	logout() {
+		button logoutButton = new JButton(new ActionListener() {
+			
+			public void ActionPerformed(ActionEvent theEvent) {
+				myPanel.hide();
+			}
+		});		
+	}
+	
+}
