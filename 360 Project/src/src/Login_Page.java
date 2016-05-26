61616a2ec6a8b3ee57887cfe4f0b6f40dde4a84f
sukhjawandhaz <sukhjawandhaz@gmail.com>
2016-05-25 17:44:35 -0700

    Update Login_Page.java

--- 360 Project/src/src/Login_Page.java
+++ 360 Project/src/src/Login_Page.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -16,21 +18,17 @@ import javax.swing.JTextField;
 
 /**
  * This class is the Login_Page GUI. It is the jumping off point for the whole GUI system, with users logging with this page and then
- * being shown a user specific page. Login attemps are checked by communicating with the Login_Database class.
+ * being shown a user specific page. Login attempts are checked by communicating with the Login_Database class.
  * 
 <<<<<<< HEAD
  * @author Dovi Bergman
  * @since 5/24/2016
 =======
  * @author Dovi Bergman, Sukhpreet Jawandha
- * @version Last Modified 5/24/2016
+ * @version Last Modified 5/25/2016
 >>>>>>> branch 'master' of https://github.com/Elthias/TeamX
  */
 public class Login_Page {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	/**
 	 * Width for the frame.
 	 */
@@ -41,29 +39,72 @@ public class Login_Page {
 	 */
 	private static final int HEIGHT = 600;
 
+	/**
+	 * Login Database to login.
+	 */
 	private final Login_Database myLogin_Database;
-
+	
+	/**
+	 * Frame to add panel.
+	 */
 	private final JFrame myFrame;
+	
+	/**
+	 * Main panel for Login Page with title name.
+	 */
 	private final JPanel myP;
+	
+	/**
+	 * Panel to cover the space.
+	 */
 	private final JPanel myP1;
+	
+	/**
+	 * Panel for Account Login and password fields and labels.
+	 */
 	private final JPanel myP2;
+	
+	/**
+	 * Panel for Login Button.
+	 */
 	private final JPanel myP3;
-	private final JPasswordField myPassField;
+	
+	/**
+	 * Password field for password.
+	 */
+	private JPasswordField myPassField;
+	
+	/**
+	 * Text field for account number.
+	 */
 	private final JTextField myIDField;
 	
+	/**
+	 * Integer to login with ID.
+	 */
 	private int myLoginID;
+	
+	/**
+	 * Password as a string.
+	 */
 	private String myPass;
-
+	
+	
+	/**
+	 * Constructor of Login page. It initialize all fields and call some methods.
+	 */
 	public Login_Page() {
 		myLoginID = 0;
 		myPass = null;
-		
 		myFrame = new JFrame();
 		setFrame();
 		myP = new JPanel();
 		myP1 = new JPanel();
 		myP2 = new JPanel();
 		myP3 = new JPanel();
+		myPassField = new JPasswordField();
+		myIDField = new JTextField("", 12);
+		myPassField = new JPasswordField(12);
 		
 		myLogin_Database = new Login_Database();
 			
@@ -94,19 +135,25 @@ public class Login_Page {
 
 	}
 
+	/**
+	 * It prints the title on the top of the panel with size and style.
+	 */
 	private void namePanel() {
 		final JLabel title = new JLabel("Clark County Library");
 		title.setFont(new Font("Serif", Font.BOLD, 40));
 		title.setForeground(Color.WHITE);
 		myP1.add(title, BorderLayout.NORTH);
 	}
-
+	
+	/**
+	 * creates the login fields and labels applied action listeners.
+	 */
 	private void login() {
 		final JLabel account = new JLabel("               Account Number :");
 		account.setFont(new Font("Serif", Font.BOLD, 25));
 		account.setForeground(Color.WHITE);
 		myP2.add(account);
-		myIDField = new JTextField("", 12);
+		
 		myIDField.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent theEvent) {
 				myLoginID = Integer.parseInt(myIDField.getText());
@@ -118,8 +165,9 @@ public class Login_Page {
 		pass.setFont(new Font("Serif", Font.BOLD, 25));
 		pass.setForeground(Color.WHITE);
 		myP2.add(pass);
-		myPassField = new JPasswordField(12);
+		
 		myPassField.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent theEvent) {
 				myPass = myPassField.getText();
 			}
@@ -128,13 +176,21 @@ public class Login_Page {
 		
 	} 
 
+	/**
+	 * creates the login button on the bottom of the login page.
+	 */
 	private void signIn() {
 		JButton login = new JButton("Login");
 		login.addActionListener(new LoginAction(myLoginID, myPass));
 		myP3.add(login);
 	}
 	
-	
+	/**
+	 * Class for login actions implementing action listenes and apply on button or fields.
+	 * 
+	 * @author dovi
+	 *
+	 */
 	public class LoginAction implements ActionListener {
 		
 		private int myID;
@@ -176,6 +232,7 @@ public class Login_Page {
 			myPassField.setText(null);
 			myIDField.setText(null);
 			//reset the text fields
+			return myValidLogin;
 			
 			if (theRole == "judge") {
 				Judge_Page jPage = new Judge_Page();
