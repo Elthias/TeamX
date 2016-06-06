06df502d3b40a185361ae3e747b598707f681e82
dovi.bergman@gmail.com <dovi.bergman@gmail.com>
2016-06-06 10:38:10 -0700

    Comments added for entrydatabase test

--- 360 Project/src/src/Entry_DatabaseTest.java
+++ 360 Project/src/src/Entry_DatabaseTest.java
@@ -1,3 +1,8 @@
+/**
+ * This is a JUnit test class for Entry_Database. It tests the only slightly complex method, setCat.
+ * @author Dovi Bergman
+ * @version 6/6/2015
+ */
 package src;
 
 import static org.junit.Assert.*;
@@ -6,6 +11,9 @@ import org.junit.Test;
 
 public class Entry_DatabaseTest {
 
+	/**
+	 * A test for setCat method. set the category of an Entry and sees if the result was achieved.
+	 */
 	@Test
 	public void setCatTest() {
 		Entry_Database db = new Entry_Database();
